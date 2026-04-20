26f24381436db66396a5e6badf14fcbeda0c12a9
anacarvalhoteixeira-ai <ana.carvalhoteixeira@ba.estudante.senai.br>
2026-04-20 15:12:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+ // HOME
+if (document.getElementById('btn-busca')) {
+    const btn = document.getElementById('btn-busca');
+    const link = document.getElementById('link-detalhes');
+    const input = document.getElementById('campo-busca');
+
+    link.style.display = 'none';
+
+    btn.addEventListener('click', () => {
+        const nome = input.value.toLowerCase();
+
+        btn.disabled = true;
+
+        fetch(`https://pokeapi.co/api/v2/pokemon/${nome}`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Erro');
+                }
+                return response.json();
+            })
+            .then(data => {
+                link.href = `sobre.html?nome=${nome}`;
+                link.style.display = 'inline-block';
+            })
+            .catch(() => {
+                alert('Pokémon não encontrado');
+                link.style.display = 'none';
+            })
+            .finally(() => {
+                btn.disabled = false;
+            });
+    });
+}
+
+// DETALHES
+if (window.location.pathname.includes('sobre.html')) {
+    const params = new URLSearchParams(window.location.search);
+    const nome = params.get('nome');
+
+    const container = document.querySelector('p');
+
+    if (nome) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${nome}`)
+            .then(res => res.json())
+            .then(data => {
+                container.innerHTML = `
+                    <div id="info">
+                        <h2>${data.name}</h2>
+                        <img src="${data.sprites.front_default}" alt="${data.name}">
+                        <span class="dado">Altura: ${data.height}</span>
+                        <span class="dado">Peso: ${data.weight}</span>
+                    </div>
+                `;
+            })
+            .catch(() => {
+                container.innerText = 'Erro ao carregar';
+            });
+    }
+}
